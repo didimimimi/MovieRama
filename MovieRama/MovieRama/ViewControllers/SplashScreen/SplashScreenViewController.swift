2ab986677ee48b40ddaf1282eab89f1c10a276e6
Dimitris Tasios <dtasios4@gmail.com>
2023-10-25 20:38:59 +0300

    feat: add splashscreen as a cover to call first api call

--- MovieRama/MovieRama/ViewControllers/SplashScreen/SplashScreenViewController.swift
+++ MovieRama/MovieRama/ViewControllers/SplashScreen/SplashScreenViewController.swift
@@ -11,7 +11,7 @@ class SplashScreenViewController: UIViewController {
     
     let backgroundView: UIView = {
         let view = UIView()
-        view.backgroundColor = .white
+        view.backgroundColor = MovieRamaConstants().CYAN_COLOR
         view.translatesAutoresizingMaskIntoConstraints = false
         return view
     }()
@@ -37,38 +37,47 @@ class SplashScreenViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        addSubviewsToView()
+        addConstraintsToSubviews()
+        animateSplashScreen()
+    }
+    
+    private func addSubviewsToView() {
         view.addSubview(backgroundView)
         view.addSubview(movieLabel)
         view.addSubview(ramaLabel)
-        
+    }
+    
+    private func addConstraintsToSubviews() {
         NSLayoutConstraint.activate([
             backgroundView.topAnchor.constraint(equalTo: view.topAnchor),
             backgroundView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
             backgroundView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
             backgroundView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
             
-            movieLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            movieLabel.trailingAnchor.constraint(equalTo: view.centerXAnchor),
             movieLabel.centerYAnchor.constraint(equalTo: view.centerYAnchor),
             
-            ramaLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            ramaLabel.leadingAnchor.constraint(equalTo: view.centerXAnchor),
             ramaLabel.centerYAnchor.constraint(equalTo: view.centerYAnchor)
         ])
-        
-        movieLabel.center.x = -view.bounds.width - movieLabel.bounds.width / 2
-        ramaLabel.center.x = view.bounds.width + ramaLabel.bounds.width / 2
-        
-        UIView.animate(withDuration: 1.0, delay: 0, options: .curveEaseOut, animations: {
-            self.movieLabel.center.x = self.view.center.x - (self.ramaLabel.bounds.width / 2)
-        }) { _ in
-            UIView.animate(withDuration: 1.0, delay: 1.0, options: .curveEaseOut, animations: {
-                self.ramaLabel.center.x = self.view.center.x + (self.ramaLabel.bounds.width / 2)
-            }) { _ in
-                let mainViewController = MovieListViewController()
-                mainViewController.modalPresentationStyle = .fullScreen
-                self.present(mainViewController, animated: false, completion: nil)
-            }
-        }
-        
     }
     
+    private func animateSplashScreen() {
+        UIView.animate(withDuration: 0, delay: 1, animations: {
+            self.backgroundView.backgroundColor = .black
+        }) { _ in
+            UIView.animate(withDuration: 0, delay: 1, animations: {
+                self.backgroundView.backgroundColor = .white
+            }) { _ in
+                self.presentMainScreen()
+            }
+        }
+    }
+    
+    private func presentMainScreen() {
+        let mainViewController = MovieListViewController()
+        mainViewController.modalPresentationStyle = .fullScreen
+        self.present(mainViewController, animated: false, completion: nil)
+    }
 }
