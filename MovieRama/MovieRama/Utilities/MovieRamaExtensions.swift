a5ae5a08c6c0e435d341b9afe5d104fe263ad038
Dimitris Tasios <dtasios4@gmail.com>
2023-10-29 08:14:01 +0200

    feat: add done button to textfield keyboard in order to dismiss it

--- MovieRama/MovieRama/Utilities/MovieRamaExtensions.swift
+++ MovieRama/MovieRama/Utilities/MovieRamaExtensions.swift
@@ -44,3 +44,23 @@ extension UIViewController {
         self.present(alertController, animated: true)
     }
 }
+
+extension UITextField {
+    func addDoneButtonOnKeyboard(){
+        let doneToolbar: UIToolbar = UIToolbar(frame: CGRect.init(x: 0, y: 0, width: UIScreen.main.bounds.width, height: 50))
+        doneToolbar.barStyle = .default
+        
+        let flexSpace = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+        let done = UIBarButtonItem(barButtonSystemItem: UIBarButtonItem.SystemItem.done, target: self, action: #selector(self.doneButtonAction))
+        
+        let items = [flexSpace, done]
+        doneToolbar.items = items
+        doneToolbar.sizeToFit()
+        
+        self.inputAccessoryView = doneToolbar
+    }
+    
+    @objc func doneButtonAction(){
+        self.resignFirstResponder()
+    }
+}
